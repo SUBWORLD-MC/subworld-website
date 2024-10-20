f0b0c9d013798caa71edab882e215fb306f11c62
flaim <105062508+flaim4@users.noreply.github.com>
2024-10-20 23:53:45 +0300

    1.4v

---
+++ main.js
@@ -0,0 +1,67 @@
+window.addEventListener('load', () => {
+    document.querySelector('.container-header').classList.add('visible');
+    document.querySelector('.main-main').classList.add('visible');
+    document.querySelector('.textmain').classList.add('animate');
+});
+
+const check = document.querySelector('.check');
+const ktomi1 = document.querySelector('.ktomi');
+const container1 = document.querySelector('.container1');
+const container2 = document.querySelector('.container2');
+const container3 = document.querySelector('.container3');
+const container4 = document.querySelector('.container4');
+const discord = document.querySelector('.discord');
+const y = document.querySelector('.y');
+const y1 = document.querySelector('.y1');
+const y2 = document.querySelector('.y2');
+const y3 = document.querySelector('.y3');
+
+function isColliding(rect1, rect2) {
+    return !(
+        rect1.right < rect2.left ||
+        rect1.left > rect2.right ||
+        rect1.bottom < rect2.top ||
+        rect1.top > rect2.bottom
+    );
+}
+
+function checkCollision() {
+    const checkRect = check.getBoundingClientRect();
+    const container1Rect = container1.getBoundingClientRect();
+    const yRect = y.getBoundingClientRect();
+    const y1Rect = y1.getBoundingClientRect();
+    const y2Rect = y2.getBoundingClientRect();
+    const y3Rect = y3.getBoundingClientRect();
+
+    if (isColliding(checkRect, container1Rect)) {
+        container2.classList.add('animated');
+    } 
+    if (isColliding(checkRect, yRect)) {
+        container2.classList.add('animated');
+        container3.classList.add('animated');
+    } 
+    if (isColliding(checkRect, y1Rect)) {
+        container2.classList.add('animated');
+        container3.classList.add('animated');
+        container4.classList.add('animated');
+    } 
+    if (isColliding(checkRect, y2Rect)) {
+        container2.classList.add('animated');
+        container3.classList.add('animated');
+        container4.classList.add('animated');
+        container4.classList.add('animated');
+        discord.classList.add('animated')
+    } 
+    if (isColliding(checkRect, y3Rect)) {
+        container2.classList.add('animated');
+        container3.classList.add('animated');
+        container4.classList.add('animated');
+        container4.classList.add('animated');
+        discord.classList.add('animated')
+    } 
+}
+setInterval(checkCollision, 100);
+
+setInterval(() => {
+    checkCollision()
+}, 1000);
